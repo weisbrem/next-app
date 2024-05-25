a4079da1c5fa9e7294ecb25317d59f824e956cac
weisbrem <zhernovkov_konst@mail.ru>
2024-05-26 01:30:30 +0200

    refactor: update product page styles

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import cn from 'classnames';
+
 import styles from './page.module.css';
 
 interface IGuitarPageProps {
@@ -13,30 +15,32 @@ export default function GuitarPage({ params }: IGuitarPageProps) {
       <h1 className='page-content__title title title--bigger'>Товар</h1>
       <ul className='breadcrumbs page-content__breadcrumbs'>
         <li className='breadcrumbs__item'>
-          <a className='link' href='./main.html'>
+          <Link className='link' href='/'>
             Главная
-          </a>
+          </Link>
         </li>
         <li className='breadcrumbs__item'>
-          <a className='link' href='./main.html'>
+          <Link className='link' href='/'>
             Каталог
-          </a>
+          </Link>
         </li>
         <li className='breadcrumbs__item'>
-          <a className='link'>Товар</a>
+          <Link className='link' href='#'>
+            Товар
+          </Link>
         </li>
       </ul>
-      <div className='product-container'>
+      <div className={styles['product-container']}>
         <img
-          className='product-container__img'
+          className={styles['product-container__img']}
           src='/img/content/catalog-product-2.jpg'
           width='90'
           height='235'
           alt='СURT Z30 Plus'
         />
-        <div className='product-container__info-wrapper'>
-          <h2 className='product-container__title title title--big title--uppercase'>СURT Z30 Plus</h2>
-          <div className='rate product-container__rating'>
+        <div className={styles['product-container__info-wrapper']}>
+          <h2 className={cn('title title--big title--uppercase', styles['product-container__title'])}>СURT Z30 Plus</h2>
+          <div className={cn('rate', styles['product-container__rating'])}>
             <svg width='14' height='14' aria-hidden='true'>
               <use xlinkHref='/img/sprite_auto.svg#icon-full-star'></use>
             </svg>
@@ -54,31 +58,31 @@ export default function GuitarPage({ params }: IGuitarPageProps) {
             </svg>
             <p className='visually-hidden'>Оценка: Хорошо</p>
           </div>
-          <div className='tabs'>
-            <a className='button button--medium tabs__button' href='#characteristics'>
+          <div className={styles['tabs']}>
+            <button type='button' className={cn('button button--medium', styles['tabs__button'])}>
               Характеристики
-            </a>
-            <a className='button button--black-border button--medium tabs__button' href='#description'>
+            </button>
+            <button type='button' className={cn('button button--medium button--black-border', styles['tabs__button'])}>
               Описание
-            </a>
-            <div className='tabs__content' id='characteristics'>
-              <table className='tabs__table'>
+            </button>
+            <div className={styles['tabs__content']}>
+              <table className={styles['tabs__table']}>
                 <tbody>
-                  <tr className='tabs__table-row'>
-                    <td className='tabs__title'>Артикул:</td>
-                    <td className='tabs__value'>SO754565</td>
+                  <tr className={styles['tabs__table-row']}>
+                    <td className={styles['tabs__title']}>Артикул:</td>
+                    <td className={styles['tabs__value']}>SO754565</td>
                   </tr>
-                  <tr className='tabs__table-row'>
-                    <td className='tabs__title'>Тип:</td>
-                    <td className='tabs__value'>Электрогитара</td>
+                  <tr className={styles['tabs__table-row']}>
+                    <td className={styles['tabs__title']}>Тип:</td>
+                    <td className={styles['tabs__value']}>Электрогитара</td>
                   </tr>
-                  <tr className='tabs__table-row'>
-                    <td className='tabs__title'>Количество струн:</td>
-                    <td className='tabs__value'>6 струнная</td>
+                  <tr className={styles['tabs__table-row']}>
+                    <td className={styles['tabs__title']}>Количество струн:</td>
+                    <td className={styles['tabs__value']}>6 струнная</td>
                   </tr>
                 </tbody>
               </table>
-              <p className='tabs__product-description hidden'>
+              <p className={cn(styles['tabs__product-description'], 'visually-hidden')}>
                 Гитара подходит как для старта обучения, так и для домашних занятий или использования в полевых
                 условиях, например, в походах или для проведения уличных выступлений. Доступная стоимость, качество и
                 надежная конструкция, а также приятный внешний вид, который сделает вас звездой вечеринки.
@@ -86,12 +90,16 @@ export default function GuitarPage({ params }: IGuitarPageProps) {
             </div>
           </div>
         </div>
-        <div className='product-container__price-wrapper'>
-          <p className='product-container__price-info product-container__price-info--title'>Цена:</p>
-          <p className='product-container__price-info product-container__price-info--value'>52 000 ₽</p>
-          <a className='button button--red button--big product-container__button' href='#'>
+        <div className={styles['product-container__price-wrapper']}>
+          <p className={cn(styles['product-container__price-info'], styles['product-container__price-info--title'])}>
+            Цена:
+          </p>
+          <p className={cn(styles['product-container__price-info'], styles['product-container__price-info--value'])}>
+            52 000 ₽
+          </p>
+          <Link className={cn('button button--red button--big', styles['product-container__button'])} href='#'>
             Добавить в корзину
-          </a>
+          </Link>
         </div>
       </div>
     </>
