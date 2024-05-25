69bed38ef0ced962795b0d89850e0e16e33422b9
weisbrem <zhernovkov_konst@mail.ru>
2024-05-26 01:12:37 +0200

    refactor: update style of cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import cn from 'classnames';
+
 import styles from './page.module.css';
 
 export default function CartPage() {
@@ -21,77 +23,49 @@ export default function CartPage() {
           <span>Корзина</span>
         </li>
       </ul>
-      <div className='cart'>
-        <div className='cart-item'>
-          <button className='cart-item__close-button button-cross' type='button' aria-label='Удалить'>
-            <span className='button-cross__icon'></span>
-            <span className='cart-item__close-button-interactive-area'></span>
+      <div className={styles['cart']}>
+        <div className={styles['cart-item']}>
+          <button
+            className={cn(styles['cart-item__close-button'], styles['button-cross'])}
+            type='button'
+            aria-label='Удалить'
+          >
+            <span className={styles['button-cross__icon']}></span>
+            <span className={styles['cart-item__close-button-interactive-area']}></span>
           </button>
-          <div className='cart-item__image'>
+          <div className={styles['cart-item__image']}>
             <Image src='/img/content/catalog-product-2.jpg' fill alt='ЭлектроГитара Честер bass' />
           </div>
-          <div className='product-info cart-item__info'>
-            <p className='product-info__title'>ЭлектроГитара Честер bass</p>
-            <p className='product-info__info'>Артикул: SO757575</p>
-            <p className='product-info__info'>Электрогитара, 6 струнная</p>
+          <div className={styles['cart-item__info']}>
+            <p className={styles['product-info__title']}>ЭлектроГитара Честер bass</p>
+            <p className={styles['product-info__info']}>Артикул: SO757575</p>
+            <p className={styles['product-info__info']}>Электрогитара, 6 струнная</p>
           </div>
-          <div className='cart-item__price'>17 500 ₽</div>
-          <div className='quantity cart-item__quantity'>
-            <button className='quantity__button' aria-label='Уменьшить количество'>
+          <div className={styles['cart-item__price']}>17 500 ₽</div>
+          <div className={cn(styles['quantity'], styles['cart-item__quantity'])}>
+            <button className={styles['quantity__button']} aria-label='Уменьшить количество'>
               <svg width='8' height='8' aria-hidden='true'>
                 <use xlinkHref='/img/sprite_auto.svg#icon-minus'></use>
               </svg>
             </button>
-            <span className='quantity__amount'>1</span>
-            <button className='quantity__button' aria-label='Увеличить количество'>
+            <span className={styles['quantity__amount']}>1</span>
+            <button className={styles['quantity__button']} aria-label='Увеличить количество'>
               <svg width='8' height='8' aria-hidden='true'>
                 <use xlinkHref='/img/sprite_auto.svg#icon-plus'></use>
               </svg>
             </button>
           </div>
-          <div className='cart-item__price-total'>17 500 ₽</div>
+          <div className={styles['cart-item__price-total']}>17 500 ₽</div>
         </div>
-        <div className='cart-item'>
-          <button className='cart-item__close-button button-cross' type='button' aria-label='Удалить'>
-            <span className='button-cross__icon'></span>
-            <span className='cart-item__close-button-interactive-area'></span>
-          </button>
-          <div className='cart-item__image'>
-            <Image
-              src='/img/content/catalog-product-4.jpg'
-              fill
-              sizes='(max-width: 767px) 100vw, (max-width: 1300px) 50vw, 33vw'
-              alt='СURT Z30 Plus'
-            />
-          </div>
-          <div className='product-info cart-item__info'>
-            <p className='product-info__title'>СURT Z30 Plus</p>
-            <p className='product-info__info'>Артикул: SO754565</p>
-            <p className='product-info__info'>Электрогитара, 6 струнная</p>
-          </div>
-          <div className='cart-item__price'>34 500 ₽</div>
-          <div className='quantity cart-item__quantity'>
-            <button className='quantity__button' aria-label='Уменьшить количество'>
-              <svg width='8' height='8' aria-hidden='true'>
-                <use xlinkHref='/img/sprite_auto.svg#icon-minus'></use>
-              </svg>
-            </button>
-            <span className='quantity__amount'>1</span>
-            <button className='quantity__button' aria-label='Увеличить количество'>
-              <svg width='8' height='8' aria-hidden='true'>
-                <use xlinkHref='/img/sprite_auto.svg#icon-plus'></use>
-              </svg>
-            </button>
-          </div>
-          <div className='cart-item__price-total'>34 500 ₽</div>
-        </div>
-        <div className='cart__footer'>
-          <div className='cart__total-info'>
-            <p className='cart__total-item'>
-              <span className='cart__total-value-name'>К оплате:</span>
-              <span className='cart__total-value cart__total-value--payment'>49 000 ₽</span>
+        <div className={styles['cart__footer']}>
+          <div className={styles['cart__total-info']}>
+            <p className={styles['cart__total-item']}>
+              <span className={styles['cart__total-value-name']}>К оплате:</span>
+              <span className={cn(styles['cart__total-value'], styles['cart__total-value--payment'])}>49 000 ₽</span>
             </p>
-            <button className='button button--red button--big cart__order-button'>Оформить заказ</button>
+            <button className={cn('button button--red button--big', styles['cart__order-button'])}>
+              Оформить заказ
+            </button>
           </div>
         </div>
       </div>
