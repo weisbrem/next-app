d515af692c892189af29a7f1f9ddce7ac20a2631
weisbrem <zhernovkov_konst@mail.ru>
2024-05-27 12:14:08 +0200

    feat: add cart work from catalog list

--- app/components/product-list/ProductList.tsx
+++ app/components/product-list/ProductList.tsx
@@ -9,13 +9,17 @@ import { productStore } from '@/stores/product.store';
 import Rating from '@/app/components/rating/Rating';
 
 import styles from './ProductList.module.css';
+import { cartStore } from '@/stores/cart.store';
 
 export default function ProductList() {
   const router = useSearchParams();
 
+  const products = productStore((state) => state.products);
   const fetchProducts = productStore((state) => state.fetchProducts);
   const getNumberOfTotalPages = productStore((state) => state.getNumberOfTotalPages);
-  const products = productStore((state) => state.products);
+
+  const setCurrentOrder = cartStore((state) => state.setCurrentOrder);
+  const orders = cartStore((state) => state.orders);
 
   const currentPage = router.get('page') ?? '1';
 
@@ -29,30 +33,44 @@ export default function ProductList() {
 
   return (
     <div className={cn(styles['cards'], styles['catalog__cards'])}>
-      {products.map(({ id, title, previewImg, price, rating }) => (
-        <div className={styles['product-card']} key={id}>
-          <img src={previewImg} width='75' height='190' alt={title} />
-          <div className={styles['product-card__info']}>
-            <div className={cn('rate', styles['product-card__rate'])}>
-              <Rating rating={rating} />
-            </div>
+      {products.map((product) => {
+        const { id, title, previewImg, price, rating } = product;
 
-            <p className={styles['product-card__title']}>{title}</p>
-            <p className={styles['product-card__price']}>
-              <span className='visually-hidden'>Цена:</span>
-              {price.toLocaleString('ru-RU')} ₽
-            </p>
+        return (
+          <div className={styles['product-card']} key={id}>
+            <img src={previewImg} width='75' height='190' alt={title} />
+            <div className={styles['product-card__info']}>
+              <div className={cn('rate', styles['product-card__rate'])}>
+                <Rating rating={rating} />
+              </div>
+
+              <p className={styles['product-card__title']}>{title}</p>
+              <p className={styles['product-card__price']}>
+                <span className='visually-hidden'>Цена:</span>
+                {price.toLocaleString('ru-RU')} ₽
+              </p>
+            </div>
+            <div className={styles['product-card__buttons']}>
+              <Link className='button button--mini' href={`/product/${id}`}>
+                Подробнее
+              </Link>
+              {orders && id in orders ? (
+                <Link href='/cart' className='button button--red-border button--mini button--in-cart'>
+                  В Корзине
+                </Link>
+              ) : (
+                <button
+                  onClick={() => setCurrentOrder(product)}
+                  type='button'
+                  className='button button--red button--mini button--add-to-cart'
+                >
+                  Купить
+                </button>
+              )}
+            </div>
           </div>
-          <div className={styles['product-card__buttons']}>
-            <Link className='button button--mini' href={`/product/${id}`}>
-              Подробнее
-            </Link>
-            <Link className='button button--red button--mini button--add-to-cart' href='#'>
-              Купить
-            </Link>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
