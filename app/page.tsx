3094c3f95dbd30c6d1382d0da64364364235aed7
weisbrem <zhernovkov_konst@mail.ru>
2024-05-27 12:15:05 +0200

    refactor: small refactors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import ProductList from '@/app/components/product-list/ProductList';
-
-import Pagination from './components/pagination/Pagination';
+import Pagination from '@/app/components/pagination/Pagination';
 
 export default function Home() {
   return (
